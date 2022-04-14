4a54d8cb8cc35f8decaba52ca39f187479b1d8ed
SimeunovicStef <eskimofrogster@gmail.com>
2022-04-14 18:42:33 +0200

    functions end + test

--- go-fiber-mongo-hrms/main.go
+++ go-fiber-mongo-hrms/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -19,10 +20,10 @@ type MongoInstance struct {
 var mg MongoInstance
 
 const dbName = "fiber-hrms"
-const mongoURI = "mongodb://localhost:27017" + dbName
+const mongoURI = "mongodb://localhost:27017/" + dbName
 
 type Employee struct {
-	ID     string  `json:"id,omitempty" bson: "_id, omitempty"`
+	ID     string  `json:"id,omitempty" bson: "_id,omitempty"`
 	Name   string  `json:"name"`
 	Salary float64 `json: "salary"`
 	Age    float64 `json: "age"`
@@ -97,6 +98,63 @@ func main() {
 
 	})
 
-	app.Put("/employee/:id")
-	app.Delete("/employee/:id")
+	app.Put("/employee/:id", func(c *fiber.Ctx) error {
+		idParam := c.Params("id")
+
+		employeeID, err := primitive.ObjectIDFromHex(idParam)
+
+		if err != nil {
+			return c.SendStatus(400)
+		}
+
+		employee := new(Employee)
+
+		if err := c.BodyParser(employee); err != nil {
+			return c.Status(400).SendString(err.Error())
+		}
+
+		query := bson.D{{Key: "_id", Value: employeeID}}
+		update := bson.D{
+			{Key: "&set",
+				Value: bson.D{
+					{Key: "name", Value: employee.Name},
+					{Key: "age", Value: employee.Age},
+					{Key: "salary", Value: employee.Salary},
+				},
+			},
+		}
+		err = mg.Db.Collection("employees").FindOneAndUpdate(c.Context(), query, update).Err()
+
+		if err != nil {
+			if err == mongo.ErrNoDocuments {
+				return c.SendStatus(400)
+			}
+			return c.SendStatus(500)
+		}
+		employee.ID = idParam
+		return c.Status(200).JSON(employee)
+	})
+
+	app.Delete("/employee/:id", func(c *fiber.Ctx) error {
+		employeeID, err := primitive.ObjectIDFromHex(c.Params("id"))
+
+		if err != nil {
+			return c.SendStatus(400)
+		}
+
+		query := bson.D{{Key: "_id", Value: employeeID}}
+		result, err := mg.Db.Collection("employees").DeleteOne(c.Context(), &query)
+
+		if err != nil {
+			return c.SendStatus(500)
+		}
+
+		if result.DeletedCount < 1 {
+			return c.SendStatus(404)
+		}
+
+		return c.Status(200).JSON("record deleted")
+	})
+
+	log.Fatal(app.Listen(":300"))
 }
